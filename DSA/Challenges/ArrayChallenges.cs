e90e13117508e2bb5460cf58fbc079568d4c8c8b
Zscoob <james.zobian@gmail.com>
2020-05-05 16:53:16 -0500

    added ArrayChallenges.cs, killed main

---
+++ DSA/Challenges/ArrayChallenges.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Challenges
+{
+
+
+    public class ArrayChallenges
+    {
+      
+        public static void EsreverYarra(string[] args)
+        {
+            {
+                int[] arr = new int[] { 1, 2, 3, 4, 5 };
+                int length = arr.Length - 1;
+                string strReverse = null;
+                while (length >= 0)
+                {
+                    strReverse = strReverse + arr[length];
+                    length--;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Reverse Array is " + " " + strReverse);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
