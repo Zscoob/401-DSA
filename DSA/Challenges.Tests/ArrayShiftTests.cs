f7a93a8f49a36976b1ea93b4220b9a4caf374efa
Zscoob <james.zobian@gmail.com>
2020-05-05 21:38:50 -0500

    tests passing

---
+++ DSA/Challenges.Tests/ArrayShiftTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+
+namespace Challenges.Test
+{
+
+    public class ArrayShiftTests
+    {
+        [Theory]
+        [InlineData(new[] { 0, 1, 2, 3 }, new[] { 0, 1, 5, 2, 3 })]
+        public void shift(int[] input, int[] expected)
+        {
+            //arrange
+            int numInput = 5;
+            //act
+            int[] result = ArrayChallenges.shiftedArr(input, numInput);
+            //assert
+            Assert.Equal(expected, result);
+
+        }
+        [Fact]
+        public void ShiftTests()
+        {
+            //arrange
+            int[] arrInp = new int[4] { 1, 2, 3, 4 };
+            int num = 5;
+            int[] expected = new int[5] { 1, 2, 5, 3, 4 };
+
+            //act
+            int[] result = ArrayChallenges.shiftedArr(arrInp, num);
+
+            //assert
+            Assert.Equal(expected, result);
+
+        }
+
+    }
+}
--- DSA/Challenges/ArrayChallenges.cs
+++ DSA/Challenges/ArrayChallenges.cs
@@ -3,7 +3,6 @@
 namespace Challenges
 {
 
-
     public class ArrayChallenges
     {
         //function returns array of int
@@ -16,11 +15,39 @@ namespace Challenges
             for (int i = 0; i < arr.Length; i++)
             {
                 //reverses array in mirrorArray
-                mirrorArray[i] = arr[arr.Length - (1+i)];
+                mirrorArray[i] = arr[arr.Length - (1 + i)];
             }
             //retuns 
-            return mirrorArray;         
-            
+            return mirrorArray;
+
         }
+
+
+        public static int[] shiftedArr(int[] arr, int numInput)
+        {
+            //determin length to find middle index
+            int[] shifty = new int[arr.Length + 1];
+            int mIndex = arr.Length / 2;
+
+            //loop for new array
+
+            for (int i = 0; i < shifty.Length; i++)
+            {
+                if (i < mIndex)
+                {
+                    shifty[i] = arr[i];
+                }
+                else if (i == mIndex)
+                {
+                    shifty[i] = numInput;
+                }
+                else if (i > mIndex)
+                {
+                    shifty[i] = arr[i - 1];
+                }
+            }
+            return shifty;
+        }
+
     }
 }
